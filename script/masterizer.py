59974d62e3f5a79d90bbb39965d6fec8862af50b
akaRicane <philippe.guelen@gmail.com>
2020-12-18 20:32:15 +0100

    slicer is ok
    fix

--- script/masterizer.py
+++ script/masterizer.py
@@ -4,6 +4,7 @@ import copy
 import logging
 import argparse
 from pathlib import Path
+import numpy as np
 
 import matplotlib.pyplot as plt
 
@@ -32,15 +33,30 @@ def getArguments():
     return parser.parse_args()
 
 def getBandFrequencies(size:int) -> list:
+    #TODO creates musical splitting / 10 bands needed
     f0 = config.REF_KEYS_DICT['A0']
     freqList = []
     for idx in range(size):
         freqList.append(f0 * 2 ** (idx + 1))
     return freqList
 
+def getClosestIndexToTargetInArray(vect: list, target: float) -> list:
+    closestIndexes = []
+    minGap = None
+    for i, val in enumerate(vect):
+        gap = np.abs(float(val - target))
+        if gap == minGap:
+            closestIndexes.append(i)
+        elif minGap is None:
+            minGap = gap
+        elif gap < minGap:
+            closestIndexes = [i]
+            minGap = gap
+    return closestIndexes
+
 class Slicer():
     def __init__(self, size: int=config.BANDS_SLICER_SIZE, newFreqs: list=None):
-        self.size = size
+        self.size = 10  #TODO to update
         self.bands = {}
         self.freqs = None
         if newFreqs is not None: self.importNewFreqsArray(newFreqs)
@@ -48,20 +64,32 @@ class Slicer():
     def importNewFreqsArray(self, newFreqs: list):
         self.freqs = copy.deepcopy(newFreqs)
         self.freqsLenght = len(self.freqs)
-        self.updateSlicer()
+        self.updateSlicer()      
+
+    def getfAmpBandBoudaries(self, bandId: int) -> (int, int):
+        return slicer.bands[f'{bandId}']["_freqIdx"][0], \
+               slicer.bands[f'{bandId + 1}']["_freqIdx"][0] - 1
 
     def updateSlicer(self):
         _freqs = copy.deepcopy(self.freqs)
         _bands = {}
         refFreqList = getBandFrequencies(self.size)
         for idx in range(len(refFreqList)):
+            _freqIdx = getClosestIndexToTargetInArray(_freqs, refFreqList[idx])
+            if len(_freqIdx) == 1:
+                boundMin = _freqIdx[0]
+            if idx + 1 < len(refFreqList):
+                boundMax = getClosestIndexToTargetInArray(_freqs, refFreqList[idx + 1])[0] - 1
+            else:
+                boundMax = len(_freqs) 
             _bands[f"{idx}"] = {
                 "_id": idx,
+                "_freqIdx": _freqIdx,
                 "_f0": refFreqList[idx],
-                "_f": []
+                "_f": _freqs[boundMin:boundMax]
             }
-        
-        ...
+        self.freqs = copy.deepcopy(_freqs)
+        self.bands = copy.deepcopy(_bands)
 
 
 if __name__ == "__main__":
@@ -84,9 +112,11 @@ if __name__ == "__main__":
     # band slicer
     slicer = Slicer(newFreqs=source.f)
     slicer.updateSlicer()
+    
 
-
-
+    minIdx, maxIdx = slicer.getfAmpBandBoudaries(5)
+    plt.plot(slicer.bands['5']["_f"], source.fampDb[minIdx:maxIdx])
+    plt.show()
 
     source.fplot()
     template.fplot()
