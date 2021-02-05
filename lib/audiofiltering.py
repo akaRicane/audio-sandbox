aa37fce5bb82c8370347045aeaa9ef03f74b2739
akaRicane <philippe.guelen@gmail.com>
2021-02-05 02:12:24 +0100

    introducing bandpass filtering in new sandbox

--- lib/audiofiltering.py
+++ lib/audiofiltering.py
@@ -8,6 +8,8 @@ sys.path.append(os.getcwd())
 from lib import audioplot
 
 class AudioFilter():
+    # designed to be one item with one filter
+    # usage would be y_filtered = audiofiltering.applyFilter(y, **args)
     def __init__(self):
         self.coefs = (None, None)
         self.type = "empty"
@@ -21,6 +23,9 @@ class AudioFilter():
         # get HP and LP sos coefs
         # filter data 
         pass
+
+    def addBandPassFilt(self):
+        ...
 
     def impulseRespOfFilter(self):
         impulse = signal.unit_impulse(512)
@@ -44,3 +49,25 @@ class AudioFilter():
         plt.grid(which='both', axis='both')
         plt.axvline(1000, color='green') # cutoff frequency
         plt.show()
+    
+    # def bandpass_filter(data, lowcut, highcut, fs, order=5):
+    #     nyq = 0.5 * fs
+    #     low = lowcut / nyq
+    #     high = highcut / nyq
+    #     b, a = butter(order, [low, high], btype='bandpass')
+    #     filtered = lfilter(b, a, data)
+    #     return filtered
+
+    # def equalizer_10band (data, fs, gain1=0, gain2=0, gain3=0, gain4=0, gain5=0, gain6=0, gain7=0, gain8=0, gain9=0, gain10=0):
+    #     band1 = bandpass_filter(data, 20, 39, fs, order=2)* 10**(gain1/20)
+    #     band2 = bandpass_filter(data, 40, 79, fs, order=3)*10**(gain2/20)
+    #     band3 = bandpass_filter(data, 80, 159, fs, order=3)*10**(gain3/20)
+    #     band4 = bandpass_filter(data, 160, 299, fs, order=3)* 10**(gain4/20)
+    #     band5 = bandpass_filter(data, 300, 599, fs, order=3)* 10**(gain5/20)
+    #     band6 = bandpass_filter(data, 600, 1199, fs, order=3)* 10**(gain6/20)
+    #     band7 = bandpass_filter(data, 1200, 2399, fs, order=3)* 10**(gain7/20)
+    #     band8 = bandpass_filter(data, 2400, 4999, fs, order=3)* 10**(gain8/20)
+    #     band9 = bandpass_filter(data, 5000, 9999, fs, order=3)* 10**(gain9/20)
+    #     band10 = bandpass_filter(data, 10000, 20000, fs, order=3)* 10**(gain10/20)
+    #     signal = band1 + band2 + band3 + band4 + band5 + band6 + band7 + band8 + band9 + band10
+    #     return signal
