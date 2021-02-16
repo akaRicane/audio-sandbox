9414e33dc9fbbb6285c158ebab41ff040372b948
Drou <a.drouadene@gmail.com>
2021-02-16 18:23:48 +0100

    created state of the art generate sweptSine function

--- lib/audiogenerator.py
+++ lib/audiogenerator.py
@@ -1,4 +1,5 @@
 import numpy as np
+import logging
 from lib import config, tool
 
 
@@ -48,3 +49,42 @@ def generateNoisySignal(fs: int = config.SAMPLING_FREQUENCY,
     x += 0.02 * np.cos(2 * np.pi * f0 * t + .11)
     x += 0.03 * np.cos(2 * np.pi * 2000 * t)
     return t, x
+
+
+def generateSweptSine(amp: float = 0.8,
+                      f0: float = 20,
+                      f1: float = 20000,
+                      t: list = None,
+                      duration: float = config.BASIC_DURATION,
+                      fs: int = config.SAMPLING_FREQUENCY,
+                      novak: bool = False):
+    """Generates a Sweptsine, from f0 to f1, with a possibility to satisfy novaks conditions (based on https://www.ant-novak.com/publications/papers/2010_ieee_novak.pdf).
+
+    Args:
+
+    Returns:
+        
+    """
+
+    if t is None:
+        t = tool.createTemporalLinspace(fs=fs, duration=duration)
+
+    if novak is True:
+        t = None
+        L = np.floor( (f0*duration) / np.log(f1/f0) ) / f0
+        newDuration = L * np.log(f1/f0)
+        t = tool.createTemporalLinspace(fs=fs, duration=newDuration)
+        print(f'New duration of the signal has been set to {newDuration} seconds, to satisfy Novak\'s conditions')
+    else:
+        L = duration / np.log(f1 / f0)
+
+    x = amp * np.sin(2 * np.pi * f0 * L * (np.exp(t / L) - 1))
+    
+    if len(t)/fs < duration:
+        zeroPadding = np.zeros(duration*fs - len(t))
+        print(type(zeroPadding))
+        x = np.concatenate([x , zeroPadding])
+        t = tool.createTemporalLinspace(fs=fs, duration=duration)
+        print("Added Zero Padding to the signal to extend it to initial duration")
+
+    return x, t
