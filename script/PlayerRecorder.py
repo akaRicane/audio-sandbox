d053fe9711d171c47a1cc049443d67168fb12277
Drou <a.drouadene@gmail.com>
2021-02-17 21:28:15 +0100

    work on recorder + fix mergefftvector + work on sweptsine generator
    changes
    
    change file location

--- script/PlayerRecorder.py
+++ script/PlayerRecorder.py
@@ -1,4 +1,5 @@
 import pyaudio
+import os, sys
 import wave
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,12 +9,15 @@ import time
 from tkinter import TclError
 from scipy.io import wavfile
 
+sys.path.append(os.getcwd())
+from lib import config, tool, audiodsp
+
 
 # Import test file
-filename_player = 'inputs\CSC_sweep_20-20k.wav'
+filename_player = os.path.join('inputs','CSC_sweep_20-20k.wav')
 file_player = wave.open(filename_player, 'rb')
 # the file name output you want to record into
-filename_record = "recording.wav"
+filename_record = os.path.join('inputs','recording.wav')
 
 # initialize PyAudio object
 p = pyaudio.PyAudio()
@@ -44,17 +48,15 @@ frames = []  # Initialize array to store frames
 # Play the sound by writing the audio data to the stream
 while data_player != b'':
     stream.write(data_player)
-    data_player = file_player.readframes(CHUNK)
     data_recorder = stream.read(CHUNK)
+    data_player = file_player.readframes(CHUNK)
     frames.append(data_recorder)
-
 # Stop, Close and terminate the stream
 stream.stop_stream()
 stream.close()
 p.terminate()
 
 print('-----Finished Recording-----')
-
 # Open and Set the data of the WAV file
 file_recorder = wave.open(filename_record, 'wb')
 file_recorder.setnchannels(CHANNEL)
