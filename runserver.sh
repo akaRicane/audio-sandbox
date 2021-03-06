668851ab9da4de6b9ca4bce6dc907dae469231eb
Pascal Pham <pham.anhkhoapascal@gmail.com>
2021-03-06 10:25:22 +0100

    Use pyaudio lib

--- runserver.sh
+++ runserver.sh
@@ -1,10 +1,5 @@
 npm run dev &
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        ./venv/bin/python3.9 ./audiogui/project/app.py
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-        ./venv/bin/python3.9 ./audiogui/project/app.py
-elif [[ "$OSTYPE" == "msys"* ]] ; then
-        ./venv/Scripts/python.exe ./audiogui/project/app.py
+python ./audiogui/project/app.py
 else
     echo "Audio Gui run failed !! Ask Pascal"
 fi
