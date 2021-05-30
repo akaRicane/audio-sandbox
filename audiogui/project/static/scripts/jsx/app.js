20378f0c7c73a0af913e1cc5d22f59f6b1730d6f
akaRicane <philippe.guelen@gmail.com>
2021-05-30 20:39:34 +0200

    graph is now main page (again)

--- audiogui/project/static/scripts/jsx/app.js
+++ audiogui/project/static/scripts/jsx/app.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Navigation from './navigation.js';
 import Main from './main';
+import Graph from './graph';
 import '../../css/style.css';
 
 function App() {
     return (
       <div className="app">
         <Navigation />
-        <Main />
+        <Graph />
       </div>
     );
 }
