d33e97f1bb272782bf291c5e73a7f93902291a0a
akaRicane <philippe.guelen@gmail.com>
2021-05-31 22:06:17 +0200

    responsive list (except id)

--- audiogui/project/static/scripts/jsx/SineGenerator.js
+++ audiogui/project/static/scripts/jsx/SineGenerator.js
@@ -11,17 +11,45 @@ class SineGenerator extends Component {
                 {id: 0, freq: 440, gain: 1.0},
                 {id: 1, freq: 1000, gain: 0.8},
             ],
-            nSines: 2
+            nSines: 2,
+            pending_freq: 1492,
+            pending_gain: 0.123
         };
     }
 
     handleDelete(id) {
         const param = this.state.param.slice();
-
+        const newNumber = this.state.nSines - 1;
         param.splice(id, 1);
 
-        this.setState({ param: param})
+        this.setState({ param: param, nSines: newNumber});
+        this.cleanSinesIds()
+    }
 
+    handleChangeNewGain(event){
+        const gain = event.target.value;
+        this.setState({ pending_gain: gain})
+    }
+
+    handleChangeNewFreq(event){
+        const freq = event.target.value;
+        this.setState({ pending_freq: freq})
+    }
+
+    handleAddSine() {
+        const param = this.state.param.slice();
+        const newId = this.state.nSines;
+        param.push({ id: newId, freq: this.state.pending_freq, gain: this.state.pending_gain})
+        const newNumber = this.state.nSines + 1;
+        this.setState({ param: param, nSines: newNumber});
+        this.cleanSinesIds();
+    }
+
+    cleanSinesIds() {
+        const id = 0;
+        const param = this.state.param.slice();
+        param.map(sine => {sine.pos = id; id++;})
+        this.setState({ param: param})
     }
 
     render() {
@@ -30,18 +58,33 @@ class SineGenerator extends Component {
                 <br />
                 Sine Generator Motherfucker !<br />
                 <ul>
-                  {this.state.param.map(sine => 
-                    <li>
-                        id: {sine.id}{"   |   "}
-                        freq: {sine.freq}{"   Hz|   "}
-                        gain: {sine.gain}{"     |   "}
+                    {this.state.param.map(sine => 
+                        <li>
+                            <button
+                              type="button"
+                              className="p-2 my-2 bg-gray-500 text-white rounded-md"
+                              onClick={() => this.handleDelete(sine.id)}
+                              >X</button>
+                            id: {sine.id}{"   |   "}
+                            freq: {sine.freq}{"   Hz|   "}
+                            gain: {sine.gain}{"     |   "}
+                        </li>
+                    )}
+                    <form>
                         <button
                           type="button"
                           className="p-2 my-2 bg-gray-500 text-white rounded-md"
-                          onClick={() => this.handleDelete(sine.id)}
-                          >X</button>
-                    </li>
-                  )}
+                          onClick={() => this.handleAddSine()}
+                          >add</button>{"   "}
+                        freq:
+                        <input 
+                            value={this.state.pending_freq}
+                            onChange={evt => this.state.handleChangeNewFreq(evt)}/>{"Hz|   "}
+                        gain: 
+                        <input 
+                            value={this.state.pending_gain}
+                            onChange={evt => this.handleChangeNewGain(evt)}/>{"|   "}    
+                    </form>
                 </ul>
             </label>
     )}
