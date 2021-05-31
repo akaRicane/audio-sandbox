0a2fcc88d9795b90bc014fd68d783701c495afcd
akaRicane <philippe.guelen@gmail.com>
2021-05-31 19:41:43 +0200

    handleDelete sine

--- audiogui/project/static/scripts/jsx/SineGenerator.js
+++ audiogui/project/static/scripts/jsx/SineGenerator.js
@@ -7,8 +7,21 @@ class SineGenerator extends Component {
         super(props);
         this.state = {
             data: [],
-            freqs: []
+            param: [
+                {id: 0, freq: 440, gain: 1.0},
+                {id: 1, freq: 1000, gain: 0.8},
+            ],
+            nSines: 2
         };
+    }
+
+    handleDelete(id) {
+        const param = this.state.param.slice();
+
+        param.splice(id, 1);
+
+        this.setState({ param: param})
+
     }
 
     render() {
@@ -16,7 +29,20 @@ class SineGenerator extends Component {
             <label>
                 <br />
                 Sine Generator Motherfucker !<br />
-                
+                <ul>
+                  {this.state.param.map(sine => 
+                    <li>
+                        id: {sine.id}{"   |   "}
+                        freq: {sine.freq}{"   Hz|   "}
+                        gain: {sine.gain}{"     |   "}
+                        <button
+                          type="button"
+                          className="p-2 my-2 bg-gray-500 text-white rounded-md"
+                          onClick={() => this.handleDelete(sine.id)}
+                          >X</button>
+                    </li>
+                  )}
+                </ul>
             </label>
     )}
 };
