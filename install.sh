690a330507600e82ed0a16b85b8cb23c39c9ff01
akaRicane <philippe.guelen@gmail.com>
2021-06-21 21:25:07 +0200

    update install + disable pyaudio for now

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 npm install
 # TODO code may won't work if python path link to python version not supported
 # User should specify version
-python -m venv venv
+python3 -m venv venv
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
         source venv/bin/activate
@@ -14,7 +14,7 @@ else
 
 fi
 echo "Install is a success ! Processing packages ..."
-pip install wheel
-pip install -r requirements.txt
-pip install PyAudio-0.2.11-cp38-cp38-win_amd64.whl
+python3 -m pip install wheel
+python3 -m pip install -r requirements.txt
+python3 -m pip install PyAudio-0.2.11-cp39-cp39-win_amd64.whl
 echo "Packages installed ! Please run: sh.runserver.sh"
