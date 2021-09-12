697e2ab5bd1852acdf0fd83dcb6a7415aa60e335
akaRicane <philippe.guelen@gmail.com>
2021-09-12 14:03:52 +0200

    read genre if found

--- scripts/sandbox/ricanebox2.py
+++ scripts/sandbox/ricanebox2.py
@@ -11,7 +11,12 @@ for track in list(ROOT_DIR.glob('**/*.mp3')):
     print(f"-------- {meta['tags']['TIT2']} --------")
     print(meta["streaminfo"])
     print(f"BPM: {meta['tags']['TBPM']}")
+    try:    
+        print(f"GENRE: {meta['tags']['TCON']}")
+    except:
+        print("no genre")
     print("\n\n")
+
 
 
 print("Job done ... Quitting !")
